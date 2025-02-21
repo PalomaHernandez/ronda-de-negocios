4905000741064dde20b587ccbdd8032db421fbe4
PalomaHernandez <palohernandez01@gmail.com>
2025-02-21 17:00:13 -0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rondas UNS Admin</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
